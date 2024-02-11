169f0dc59134f4a5361861040aca54a55ce498d3
Walker Alfaro Trelles <dev.walkeralfaro@gmail.com>
2024-02-11 13:05:38 -0500

    add confetti emojis

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 document.addEventListener('DOMContentLoaded', function(){
 
+  const jsConfetti = new JSConfetti();
   
+  const yes_btn = document.querySelector('#siBtn');
+
+  const confettiValues = { 
+    emojis: ['🌈', '💋', '😘', '😍', '🥰', '💖', '💗'],
+    emojiSize: 100,
+    confettiNumber: 70,
+  }
 
 
 
+  yes_btn.addEventListener('click', repitConfetti);
+
+  function repitConfetti() {
+    yesEvents();
+    setInterval(yesEvents, 2500)
+  }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  function yesEvents() {
+    if(screen.width > 430) {
+      confettiValues.emojiSize = 40;
+    } else {
+      confettiValues.emojiSize = 100;
+    }
+    jsConfetti.addConfetti(confettiValues);
+  }
 
 
 
